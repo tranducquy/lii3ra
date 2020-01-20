a7f05ac2f662dc1b940f2563fd59a8b5349cf25c
1011.mbp2016 <takeyuki.t@gmail.com>
2020-01-20 17:13:17 +0900

    20200120

--- lii3ra/exit_strategy/sigma1.py
+++ lii3ra/exit_strategy/sigma1.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 from lii3ra.ordertype import OrderType
 from lii3ra.technical_indicator.bollingerband import Bollingerband
@@ -49,11 +50,16 @@ class Sigma1(ExitStrategy):
                  , ohlcv
                  , span
                  , sigma1_ratio
+                 , num_of_bars_long
+                 , num_of_bars_short
                  , losscut_ratio=0.05):
-        self.title = f"Sigma1[{span:.0f},{sigma1_ratio:.2f}]"
+        self.title = f"Sigma1[{span:.0f},{sigma1_ratio:.2f}][{num_of_bars_long:0.f},{num_of_bars_short:.0f}]"
         self.ohlcv = ohlcv
         self.symbol = ohlcv.symbol
         self.bb = Bollingerband(ohlcv, span, sigma1_ratio)
+        self.sigma1_ratio = sigma1_ratio
+        self.num_of_bars_long = num_of_bars_long
+        self.num_of_bars_short = num_of_bars_short
         self._last_price = 0
         self.pos_price = 0
         self.losscut_ratio = losscut_ratio
@@ -77,12 +83,14 @@ class Sigma1(ExitStrategy):
     def create_order_exit_long_stop_market(self, idx, entry_idx):
         if not self._is_valid(idx):
             return 0.00
-        price = self.bb.lower_sigma1[idx]
+        # price = self.bb.lower_sigma1[idx]
+        num_of_bars = idx - entry_idx
+        price = self.bb.sma - (self.sigma[idx] * self.sigma1_ratio / num_of_bars)
         # 最低losscut設定
         losscut_price = self.pos_price - (self.pos_price * self.losscut_ratio)
         if price < losscut_price:
             price = losscut_price
-        return price
+        return math.floor(price)
 
     def create_order_exit_short_stop_market(self, idx, entry_idx):
         if not self._is_valid(idx):
